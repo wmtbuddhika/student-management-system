4bbdfb0564541387e711abf8aaf07e6fdb8e1696
Thushara <thushara@electrify.sg>
2020-04-29 13:17:50 +0530

    role id change

--- pages/navigation/side_menu.php
+++ pages/navigation/side_menu.php
@@ -4,7 +4,7 @@
     if($userType == 1) {
         require_once('pages/navigation/side_menu-admin.php');
     } elseif ($userType == 2) {
-        require_once('pages/navigation/side_menu-student.php');
-    } elseif ($userType == 3) {
         require_once('pages/navigation/side_menu-tutor.php');
+    } elseif ($userType == 3) {
+        require_once('pages/navigation/side_menu-student.php');
     }
