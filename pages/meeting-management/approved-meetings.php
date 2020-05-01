ad0ee85235fe39b8027d630eb57e977c178cf635
Thushara <thushara@electrify.sg>
2020-05-01 15:10:33 +0530

    meeting with group complete

--- pages/meeting-management/approved-meetings.php
+++ pages/meeting-management/approved-meetings.php
@@ -21,46 +21,56 @@
                         </thead>
                         <tbody>
                         <?php
-                        $query = "SELECT m.id,a.code,m.title,m.schedule_date,m.status FROM allocation_group a,meeting m 
-                                  WHERE a.id = m.allocation_id AND a.status = 1 AND m.status != 0 AND a.id IN ($allocationId)";
-                        $meetings = mysqli_query($con, $query);
-                        while ($meeting = mysqli_fetch_array($meetings)) {
-                            ?>
-
-                            <tr>
-                                <td class="text-center"><?php echo $meeting['code']; ?></td>
-                                <td><strong><?php echo $meeting['title']; ?></strong></td>
-                                <td><?php echo $meeting['schedule_date']; ?></td>
-                                <td><?php if($meeting['status'] == 0) {echo 'Pending';} elseif ($meeting['status'] == 1) {echo 'Scheduled';} elseif ($meeting['status'] == 2) {echo 'Canceled';} ?></td>
-                                <?php
-                                $userType = $_SESSION['user_type'];
-                                if ($userType != 3) {
-                                    ?>
-                                    <td>
-                                        <button id="<?php echo $meeting['id']; ?>"
-                                                class="btn btn-default btn-rounded btn-condensed btn-sm"
-                                                onclick="loadMeeting(this.id)"><span class="fa fa-pencil"></span>
-                                        </button>
-                                        <button id="<?php echo $meeting['id']; ?>"
-                                                class="btn btn-default btn-rounded btn-condensed btn-sm"
-                                                onclick="loadMinutes(this.id)"><span class="fa fa-book"></span>
-                                        </button>
-                                    </td>
-                                    <?php
-                                } else {
-                                    ?>
-                                    <td>
-                                        <button id="<?php echo $meeting['id']; ?>"
-                                                class="btn btn-default btn-rounded btn-condensed btn-sm"
-                                                onclick="loadMinutes(this.id)"><span class="fa fa-book"></span>
-                                        </button>
-                                    </td>
-                                    <?php
-                                }
+                        $allocationId = $_SESSION['allocation_id'];
+                        if ($allocationId != null) {
+                            $query = "SELECT m.id,ag.code,m.title,m.schedule_date,m.status,u.name FROM allocation_group ag,meeting m, user u, allocation a
+                                  WHERE a.allocation_group_id = ag.id AND m.entered_by = u.id AND ag.id = m.allocation_id AND ag.status = 1 AND m.status != 0 AND a.status = 1
+                                  AND IF($userType = 3, a.student_id = $userId, ag.tutor_id = $userId) AND ag.id IN ($allocationId)";
+                            $meetings = mysqli_query($con, $query);
+                            while ($meeting = mysqli_fetch_array($meetings)) {
                                 ?>
-                            </tr>
 
-                            <?php
+                                <tr>
+                                    <td class="text-center"><?php echo $meeting['code']; ?></td>
+                                    <td><strong><?php echo $meeting['title']; ?></strong></td>
+                                    <td><?php echo $meeting['schedule_date']; ?></td>
+                                    <td><?php if ($meeting['status'] == 0) {
+                                            echo 'Pending';
+                                        } elseif ($meeting['status'] == 1) {
+                                            echo 'Scheduled';
+                                        } elseif ($meeting['status'] == 2) {
+                                            echo 'Canceled';
+                                        } ?></td>
+                                    <?php
+                                    $userType = $_SESSION['user_type'];
+                                    if ($userType != 3) {
+                                        ?>
+                                        <td>
+                                            <button id="<?php echo $meeting['id']; ?>"
+                                                    class="btn btn-default btn-rounded btn-condensed btn-sm"
+                                                    onclick="loadMeeting(this.id)"><span class="fa fa-pencil"></span>
+                                            </button>
+                                            <button id="<?php echo $meeting['id']; ?>"
+                                                    class="btn btn-default btn-rounded btn-condensed btn-sm"
+                                                    onclick="loadMinutes(this.id)"><span class="fa fa-book"></span>
+                                            </button>
+                                        </td>
+                                        <?php
+                                    } else {
+                                        ?>
+                                        <td>
+                                            <button id="<?php echo $meeting['id']; ?>"
+                                                    class="btn btn-default btn-rounded btn-condensed btn-sm"
+                                                    onclick="loadMinutes(this.id)"><span class="fa fa-book"></span>
+                                            </button>
+                                        </td>
+                                        <?php
+                                    }
+                                    ?>
+                                </tr>
+
+                                <?php
+                            }
                         }
                         ?>
                         </tbody>
--- pages/meeting-management/pending-meetings.php
+++ pages/meeting-management/pending-meetings.php
@@ -22,24 +22,29 @@
                         <tbody>
                         <?php
                         $allocationId = $_SESSION['allocation_id'];
-                        $query = "SELECT m.id,ag.code,m.title,m.schedule_date,m.status,u.name FROM allocation_group ag,meeting m, user u, allocation a
+                        if ($allocationId != null) {
+                            $query = "SELECT m.id,ag.code,m.title,m.schedule_date,m.status,u.name FROM allocation_group ag,meeting m, user u, allocation a
                                   WHERE a.allocation_group_id = ag.id AND m.entered_by = u.id AND ag.id = m.allocation_id AND ag.status = 1 AND m.status = 0 AND a.status = 1
                                   AND IF($userType = 3, a.student_id = $userId, ag.tutor_id = $userId) AND ag.id IN ($allocationId)";
-                        $meetings = mysqli_query($con, $query);
-                        while ($meeting = mysqli_fetch_array($meetings)) {
-                            ?>
+                            $meetings = mysqli_query($con, $query);
+                            while ($meeting = mysqli_fetch_array($meetings)) {
+                                ?>
 
-                            <tr id="trow_3">
-                                <td class="text-center"><?php echo $meeting['code']; ?></td>
-                                <td><strong><?php echo $meeting['title']; ?></strong></td>
-                                <td><?php echo $meeting['schedule_date']; ?></td>
-                                <td><?php echo $meeting['name']; ?></td>
-                                <td>
-                                    <button id="<?php echo $meeting['id']; ?>" class="btn btn-default btn-rounded btn-condensed btn-sm"onclick="loadMeeting(this.id)"><span class="fa fa-pencil"></span></button>
-                                </td>
-                            </tr>
+                                <tr id="trow_3">
+                                    <td class="text-center"><?php echo $meeting['code']; ?></td>
+                                    <td><strong><?php echo $meeting['title']; ?></strong></td>
+                                    <td><?php echo $meeting['schedule_date']; ?></td>
+                                    <td><?php echo $meeting['name']; ?></td>
+                                    <td>
+                                        <button id="<?php echo $meeting['id']; ?>"
+                                                class="btn btn-default btn-rounded btn-condensed btn-sm"
+                                                onclick="loadMeeting(this.id)"><span class="fa fa-pencil"></span>
+                                        </button>
+                                    </td>
+                                </tr>
 
-                            <?php
+                                <?php
+                            }
                         }
                         ?>
                         </tbody>
